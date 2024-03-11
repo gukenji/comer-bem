5fd479c74dff67efb527a47c8bf3b84c14b68387
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2024-03-11 19:14:34 -0300

    style changes in include to freezer search

--- frontend/src/components/IncludeToFreezer.tsx
+++ frontend/src/components/IncludeToFreezer.tsx
@@ -5,6 +5,7 @@ import { useAppDispatch, useAppSelector } from "../store/store";
 import { getFoods } from "../store/features/foodsSlice";
 import { useEffect } from "react";
 import { IFood, IGetFood } from "../interfaces/FoodInterfaces";
+import { Box, Divider } from "@mui/material";
 export default function IncludeToFreezer() {
   const [value, setValue] = React.useState<IGetFood | null>(null);
   const [inputValue, setInputValue] = React.useState("");
@@ -23,28 +24,53 @@ export default function IncludeToFreezer() {
   }, []);
 
   return (
-    <div>
-      <div>{`value: ${value !== null ? `'${value.id}'` : "null"}`}</div>
-      <div>{`inputValue: '${inputValue}'`}</div>
-      <br />
-      <Autocomplete
-        value={value}
-        filterSelectedOptions
-        onChange={(event, newValue: IGetFood | null) => {
-          setValue(newValue);
-        }}
-        inputValue={inputValue}
-        onInputChange={(event, newInputValue) => {
-          setInputValue(newInputValue);
-        }}
-        id="controllable-states-demo"
-        options={foods as IGetFood[]}
-        getOptionLabel={(option) => (option as { name: string }).name}
-        renderOption={(props, option) => <li {...props}>{option?.name}</li>}
-        renderInput={(params) => (
-          <TextField {...params} variant="standard" label="BUSCAR ALIMENTO" />
-        )}
-      />
-    </div>
+    <Box sx={{ p: 1 }}>
+      <div>
+        <div>{`value: ${value !== null ? `'${value.id}'` : null}`}</div>
+        <div>{`inputValue: '${inputValue}'`}</div>
+
+        <br />
+        <Autocomplete
+          value={value}
+          filterSelectedOptions
+          onChange={(event, newValue: IGetFood | null) => {
+            setValue(newValue);
+          }}
+          inputValue={inputValue}
+          onInputChange={(event, newInputValue) => {
+            setInputValue(newInputValue);
+          }}
+          id="controllable-states-demo"
+          options={foods as IGetFood[]}
+          getOptionLabel={(option) => (option as { name: string }).name}
+          renderOption={(props, option) => (
+            <>
+              <li
+                {...props}
+                style={{
+                  fontFamily: "VT323",
+                  fontSize: 20,
+                  paddingTop: 0,
+                  paddingBottom: 0,
+                  marginTop: 0,
+                  marginBottom: 0,
+                }}
+              >
+                {option?.name}
+              </li>
+              <Divider />
+            </>
+          )}
+          renderInput={(params) => (
+            <TextField
+              {...params}
+              variant="standard"
+              label="BUSCAR ALIMENTO"
+              InputLabelProps={{ sx: { fontFamily: "VT323", fontSize: 20 } }}
+            />
+          )}
+        />
+      </div>
+    </Box>
   );
 }
