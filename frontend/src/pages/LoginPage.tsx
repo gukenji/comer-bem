ccd08eedbcd606ed0e77d1af25584484e2f6a593
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2024-03-18 15:11:58 -0300

    add error messages in login

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -8,7 +8,10 @@ import {
   Box,
   Typography,
   TextField,
+  Alert,
   Grid,
+  FormHelperText,
+  CircularProgress,
 } from "@mui/material";
 import { PadlockIcon } from "../styles/PadlockIcon";
 import { LoginButton } from "../styles/LoginButton";
@@ -16,23 +19,57 @@ const LoginPage = () => {
   const dispatch = useAppDispatch();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const handleLogin = async () => {
+    setLoading(true);
     if (email && password) {
       try {
         await dispatch(login({ email, password })).unwrap();
       } catch (e) {
-        console.error(e);
+        setError(true);
+        console.log(e);
       }
     } else {
-      console.log("teste");
+      setError(true);
     }
+    setLoading(false);
   };
 
+  const helperText = (
+    <FormHelperText
+      id="standard-weight-helper-text"
+      sx={{ color: "red", fontFamily: "VT323", fontSize: 17 }}
+    >
+      PREENCHA ESTE CAMPO
+    </FormHelperText>
+  );
+  const errorMessage = (
+    <Box sx={{ width: "100%" }}>
+      {error ? (
+        <Alert
+          severity="error"
+          sx={{
+            mt: 2,
+            fontFamily: "VT323",
+            fontSize: 19,
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+          }}
+        >
+          ERRO NO LOGIN!
+        </Alert>
+      ) : (
+        <></>
+      )}
+    </Box>
+  );
   return (
-    <div>
+    <Box>
       <Container maxWidth="xs">
         <CssBaseline />
+
         <Box
           sx={{
             mt: 20,
@@ -45,7 +82,8 @@ const LoginPage = () => {
           <Typography variant="h5" sx={{ fontFamily: "VT323" }}>
             LOGIN
           </Typography>
-          <Box sx={{ mt: 1 }}>
+          {errorMessage}
+          <Box sx={{ mt: 1, width: "100%" }}>
             <TextField
               margin="normal"
               required
@@ -57,8 +95,12 @@ const LoginPage = () => {
               name="email"
               autoFocus
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              onClick={(e) => setError(false)}
             />
+            {error && email.length < 1 ? helperText : <></>}
 
             <TextField
               margin="normal"
@@ -74,21 +116,28 @@ const LoginPage = () => {
               onChange={(e) => {
                 setPassword(e.target.value);
               }}
+              onClick={(e) => setError(false)}
             />
-
+            {error && password.length < 1 ? helperText : <></>}
             <LoginButton
               fullWidth
+              className={error ? "error" : null}
+              disabled={
+                loading || password.length < 1 || email.length < 1
+                  ? true
+                  : false
+              }
               variant="contained"
               sx={{
                 mt: 3,
                 mb: 2,
                 fontFamily: "VT323",
                 fontSize: { xs: 20, md: 25 },
-                background: "#3ca370",
+                background: !error ? "#3ca370" : "#c32454",
               }}
               onClick={handleLogin}
             >
-              Login
+              {loading ? <CircularProgress color="success" /> : "LOGIN"}{" "}
             </LoginButton>
             <Grid container justifyContent={"flex-end"}>
               <Grid item>
@@ -98,7 +147,7 @@ const LoginPage = () => {
           </Box>
         </Box>
       </Container>
-    </div>
+    </Box>
   );
 };
 
--- frontend/src/styles/LoginButton.tsx
+++ frontend/src/styles/LoginButton.tsx
@@ -3,18 +3,42 @@ import { Button } from "@mui/material";
 export const LoginButton = styled(Button)(({ theme }) => ({
   "&:active": {
     [theme.breakpoints.up("xs")]: {
-      background: "#80366b",
+      background: "#3ca370",
     },
     [theme.breakpoints.up("md")]: {
-      background: "#80366b",
+      background: "#3ca370",
+    },
+  },
+  "&:active.error": {
+    [theme.breakpoints.up("xs")]: {
+      background: "#c32454",
+    },
+    [theme.breakpoints.up("md")]: {
+      background: "#c32454",
     },
   },
   "&:hover": {
     [theme.breakpoints.up("xs")]: {
-      background: "#80366b",
+      background: "#3ca370",
     },
     [theme.breakpoints.up("md")]: {
-      background: "#80366b",
+      background: "#3ca370",
+    },
+  },
+  "&:hover.error": {
+    [theme.breakpoints.up("xs")]: {
+      background: "#c32454",
+    },
+    [theme.breakpoints.up("md")]: {
+      background: "#c32454",
+    },
+  },
+  "&:disabled": {
+    [theme.breakpoints.up("xs")]: {
+      background: "#3ca370",
+    },
+    [theme.breakpoints.up("md")]: {
+      background: "#3ca370",
     },
   },
 }));
